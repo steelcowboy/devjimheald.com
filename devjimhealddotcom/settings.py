64c914504bc47bb4d2256a6bfea9e67425854146
Jim Heald <james.r.heald@gmail.com>
2017-10-29 13:42:23 -0700

    Starting the migration to PostgreSQL

--- devjimhealddotcom/settings.py
+++ devjimhealddotcom/settings.py
@@ -80,9 +80,17 @@ WSGI_APPLICATION = 'devjimhealddotcom.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'devjimheald.com',
+        'USER': 'devjim-admin',
+        'PASSWORD': secret.password,
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
     }
 }
 
