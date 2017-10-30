966924a6222194b6e594b385020236842c7e1cf8
Jim Heald <james.r.heald@gmail.com>
2017-10-29 22:42:09 -0700

    Better naming, and should also collect these files

--- devjimhealddotcom/settings.py
+++ devjimhealddotcom/settings.py
@@ -129,6 +129,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "assets"),
+]
+
 STATIC_URL = '/assets/'
 STATIC_ROOT = secret.STATIC_ROOT
 MEDIA_ROOT = secret.MEDIA_ROOT
