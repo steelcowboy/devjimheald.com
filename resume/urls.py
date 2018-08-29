1965f542ec962094114e39deb78e71b2e98909e1
Jim Heald <james.r.heald@gmail.com>
2018-08-29 08:31:32 -0700

    Set up a tag filtering system on projects and points, and switch to CBV

--- resume/urls.py
+++ resume/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('printable', views.printable, name='printable'),
-    path('printable/<str:tag>', views.printable, name='printable'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('printable', views.PrintableView.as_view(), name='printable'),
+    path('printable/<str:tag>', views.PrintableView.as_view(), name='printable_tagged'),
 ]
--- resume/views.py
+++ resume/views.py
@@ -1,27 +1,52 @@
+from typing import List, Dict, Any, Optional
+
 from django.shortcuts import render
+from django.views import View
 from .models import Skill, Project, Education, Affiliation, Employment
 
-# Create your views here.
-def index(request):
-    return render(request, 'resume/index.html')
 
-def printable(request, tag=None):
-    kwargs = {}
-    if tag is not None:
-        kwargs['tags__name__in'] = [tag]
+OptStr = Optional[str]
 
-    skills = Skill.objects.all() 
-    projects = Project.objects.filter(**kwargs)
-    educations = Education.objects.all()
-    affiliations = Affiliation.objects.filter(**kwargs)
-    employments = Employment.objects.filter(**kwargs)
 
-    context = {
-            'skills': skills, 
-            'projects': projects, 
-            'educations': educations, 
-            'affiliations': affiliations,
-            'employments': employments
+class IndexView(View):
+    def get(self, request):
+        return render(request, 'resume/index.html')
+
+class PrintableView(View):
+    NO_TAGS = [Affiliation]
+
+    def _get_section_list(self, section) -> List[Dict[str, Any]]:
+        sect_name = section.__name__.lower()
+        points_attr = f"{sect_name}point_set"
+
+        filter_args = {'archived': False}  # Filter archived objects
+        if self.tag is not None and section not in self.NO_TAGS:
+            filter_args['tags__name__in'] = [self.tag]
+        
+        return [
+            {
+                "object": obj,
+                "points": getattr(obj, points_attr).filter(**filter_args)
             }
+            for obj in section.objects.filter(**filter_args)
+        ]
 
-    return render(request, 'resume/printable.html', context)
+    def get(self, request, tag=None):
+        self.tag = tag
+
+        skills = Skill.objects.all() 
+        educations = Education.objects.all()
+
+        projects = self._get_section_list(Project) 
+        affiliations = self._get_section_list(Affiliation)
+        employments = self._get_section_list(Employment)
+
+        context = {
+                'skills': skills, 
+                'projects': projects, 
+                'educations': educations, 
+                'affiliations': affiliations,
+                'employments': employments
+                }
+
+        return render(request, 'resume/printable.html', context)
